d00328ecfb32840f71e096391c06704bc57a0c23
GuillaumeQ11 <qu.guillaume11@gmail.com>
2024-07-14 18:21:31 +0200

    doc & readme

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# url_shortener_project
+# Application de Raccourcissement d'URL avec FastAPI
+
+Cette application web permet de raccourcir des URLs longues en URLs plus courtes, similaires à des services comme Bitly ou TinyURL. L'application est développée en utilisant FastAPI et utilise une base de données SQLite pour stocker les mappings URL raccourcies.
+
+## Fonctionnalités
+
+- **Raccourcissement d'URL :** Permet aux utilisateurs de soumettre une URL longue et de recevoir en retour une URL raccourcie unique.
+- **Redirection :** Les URLs raccourcies redirigent les utilisateurs vers l'URL d'origine.
+
+## Installation
+
+1. **Clonage du dépôt :**
+
+   ```bash
+   git clone https://github.com/GuillaumeQ11/url_shortener_project.git
+   cd url_shortener_project
+   ```
+
+2. **Installation des dépendances :**
+
+   Assurez-vous que Python 3.7+ est installé. Créez un environnement virtuel et activez-le :
+
+   ```bash
+   python -m venv .venv
+   source .venv/bin/activate  # Sur Windows, utilisez .venv\Scripts\activate
+   ```
+
+   Installez les dépendances avec pip :
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+3. **Lancement de l'application :**
+
+   Démarrez l'application FastAPI :
+
+   ```bash
+   uvicorn main:app --reload
+   ```
+
+   L'application sera accessible à l'adresse par défaut : [http://localhost:8000](http://localhost:8000).
+
+## Utilisation
+
+1. Accédez à l'interface web à [http://localhost:8000](http://localhost:8000).
+
+2. Entrez une URL longue dans le champ dédié et soumettez le formulaire.
+
+3. Vous recevrez une URL raccourcie qui redirige vers l'URL d'origine.
+
+## Auteur
+
+GuillaumeQ11 - [GitHub](https://github.com/GuillaumeQ11)
--- main.py
+++ main.py
@@ -15,6 +15,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 class URL(Base):
+    """
+    Defines a SQLAlchemy table 'urls' with columns for ID, original URL, and slug.
+    """
+
     __tablename__ = "urls"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -23,30 +27,35 @@ class URL(Base):
 
 Base.metadata.create_all(bind=engine)
 
-
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 def get_db():
+    '''Get the db session'''
+
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close
 
-@app.get("/")
-def read_root():
-    file_path = os.path.join(os.path.dirname(__file__), "index.html")
-    with open(file_path, "r") as file:
-        html_content = file.read()
-        return HTMLResponse(content=html_content)
-
-def raise_bad_request(message):
+@app.get("/{slug}")
+def redirect_url(slug: str, db: Session = Depends(get_db)):
+    db_url = db.query(URL).filter(URL.slug == slug).first()
+    if db_url:
+        return RedirectResponse(url=db_url.original_url)
+    else:
+        raise raise_bad_request("URL raccourcie non trouvée")
+    
+def raise_bad_request(message) -> HTTPException:
+    '''Raise an HTTPException with status code 400 and the provided message'''
     raise HTTPException(status_code=400, detail=message)
 
 def is_url_accessible(url: str) -> bool:
+    ''' 
+    Check if the url is valid, firstly with typo validation, then trying to access to the url
+    '''
     try:
         response = requests.head(url, allow_redirects=True, timeout=5)
         return response.status_code == 200
@@ -54,9 +63,39 @@ def is_url_accessible(url: str) -> bool:
         return False
     except requests.Timeout:
         return False
+    
+def generate_unique_slug(db: Session) -> str:
+    """
+    Generate a unique 8-character slug using UUID.
+    Also checks that the slug does not already exist in the database.
+    """
+
+    while True:
+        slug = str(uuid.uuid4())[:8]
+        existing_url = db.query(URL).filter(URL.slug == slug).first()
+        db.close()
+        if not existing_url:
+            return slug
+        
+@app.get("/")
+def read_root():
+    """
+    Endpoint to read and return the content of 'index.html' file as HTML response.
+    """
+
+    file_path = os.path.join(os.path.dirname(__file__), "index.html")
+    with open(file_path, "r") as file:
+        html_content = file.read()
+        return HTMLResponse(content=html_content)
 
 @app.post("/url")
 def create_url(url: str = Form(...), db: Session = Depends(get_db)):
+    """
+    Endpoint to shorten a long URL. Validates the URL first, checks its accessibility,
+    generates a unique slug, saves the URL and slug in the database.
+    Returns a dictionary containing the shortened URL.
+    """
+
     if not validators.url(url):
         raise_bad_request(message="L'URL saisie est incorrecte")
     if is_url_accessible(url):
@@ -71,19 +110,3 @@ def create_url(url: str = Form(...), db: Session = Depends(get_db)):
        return {"short_url": f"http://localhost:8000/{slug}"}
     else:
         raise HTTPException(status_code=404, detail="L'URL n'est pas accessible")
-
-@app.get("/{slug}")
-def redirect_url(slug: str, db: Session = Depends(get_db)):
-    db_url = db.query(URL).filter(URL.slug == slug).first()
-    if db_url:
-        return RedirectResponse(url=db_url.original_url)
-    else:
-        raise raise_bad_request("URL raccourcie non trouvée")
-
-def generate_unique_slug(db: Session):
-    while True:
-        slug = str(uuid.uuid4())[:8]
-        existing_url = db.query(URL).filter(URL.slug == slug).first()
-        db.close()
-        if not existing_url:
-            return slug
